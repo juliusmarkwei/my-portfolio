8ef0042f63292d3f7123dd6c55546b60fde30824
Julius Markwei <juliusmarkwei2000@gmail.com>
2023-08-31 14:56:56 +0000

    added tabs

--- app.py
+++ app.py
@@ -6,7 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
 
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route('/bio')
+def bio():
+    return render_template('bio.html')
+
+@app.route('/podcast')
+def podcast():
+    return render_template('podcast.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
